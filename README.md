1ae79fa902ba84c3e48eeacd539c324e802ffb09
YLuzew <boyfrindgood@outlook.com>
2026-08-06 15:00:24 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# FND' - Codename Engine Build
+
+![fnd](art/dustin-header.webp)
+
+<div style="text-align:center;font-size: 20px; font-size: 1.2em;">
+
+T H E &nbsp; V E S S E L S &nbsp; A R E &nbsp; R E A D Y.
+
+A F T E R &nbsp; A L L &nbsp; T H E S E &nbsp; Y E A R S.
+
+A R E &nbsp; Y O U &nbsp; R E A D Y ?
+</div>
+
+DUSTIN MOD DOWNLOADS:
+-  **GAMEBANANA:** [https://gamebanana.com/mods/613322](https://gamebanana.com/mods/613322)
+-  **GAME JOLT:**  [https://gamejolt.com/games/fridaynightdustinFULL/1012008](https://gamejolt.com/games/fridaynightdustinFULL/1012008)
+
+# ENGINE???
+This mod is made on a extensible modding framework for FNF: Codename Engine. This source code folder can be placed in the mods folder to be played in codename!
+
+![codename logo](art/cne.jpg)
+
+Download Codename:
+- Windows: [https://tinyurl.com/cne-download-windows](https://nightly.link/CodenameCrew/CodenameEngine/workflows/windows/main/Codename%20Engine.zip)
+- Mac OS: [https://tinyurl.com/cne-download-macos](https://nightly.link/CodenameCrew/CodenameEngine/workflows/macos/main/Codename%20Engine.zip)
+- Linux: [https://tinyurl.com/cne-download-linux](https://nightly.link/CodenameCrew/CodenameEngine/workflows/linux/main/Codename%20Engine.zip)
+
+Codename's Github: [https://github.com/CodenameCrew/CodenameEngine](https://github.com/CodenameCrew/CodenameEngine)
+
+Little note, dustin builds on gamejolt come with a exe built in!!
---
+++ source/mobile/funkin/backend/utils/MobileData.hx
@@ -0,0 +1,147 @@
+/*
+ * Copyright (C) 2025 Mobile Porting Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package mobile.funkin.backend.utils;
+
+#if TOUCH_CONTROLS
+import haxe.ds.Map;
+import haxe.Json;
+import haxe.io.Path;
+import openfl.utils.Assets;
+
+/**
+ * ...
+ * @author: Karim Akra
+ */
+class MobileData
+{
+	public static var actionModes:Map<String, TouchButtonsData> = new Map();
+	public static var dpadModes:Map<String, TouchButtonsData> = new Map();
+
+	public static var mode(get, set):Int;
+	public static var forcedMode:Null<Int>;
+
+	public static function init()
+	{
+		for (folder in [
+			'${ModsFolder.modsPath}${ModsFolder.currentModFolder}/mobile',
+			Paths.getPath('mobile')
+		])
+			if (FileSystem.exists(folder) && FileSystem.isDirectory(folder))
+			{
+				setMap('$folder/DPadModes', dpadModes);
+				setMap('$folder/ActionModes', actionModes);
+			}
+	}
+
+	public static function setMap(folder:String, map:Map<String, TouchButtonsData>)
+	{
+		for (file in FileSystem.readDirectory(folder))
+		{
+			if (Path.extension(file) == 'json')
+			{
+				file = Path.join([folder, Path.withoutDirectory(file)]);
+				var str = File.getContent(file);
+				var json:TouchButtonsData = cast Json.parse(str);
+				var mapKey:String = Path.withoutDirectory(Path.withoutExtension(file));
+				map.set(mapKey, json);
+			}
+		}
+	}
+	
+	public static function setTouchPadCustom(touchPad:TouchPad):Void
+	{
+		if (FlxG.save.data.buttons == null)
+		{
+			FlxG.save.data.buttons = new Array();
+			for (buttons in touchPad)
+				FlxG.save.data.buttons.push(FlxPoint.get(buttons.x, buttons.y));
+		}
+		else
+		{
+			var tempCount:Int = 0;
+			for (buttons in touchPad)
+			{
+				FlxG.save.data.buttons[tempCount] = FlxPoint.get(buttons.x, buttons.y);
+				tempCount++;
+			}
+		}
+
+		FlxG.save.flush();
+	}
+
+	public static function getTouchPadCustom(touchPad:TouchPad):TouchPad
+	{
+		var tempCount:Int = 0;
+
+		if (FlxG.save.data.buttons == null)
+			return touchPad;
+
+		for (buttons in touchPad)
+		{
+			if (FlxG.save.data.buttons[tempCount] != null)
+			{
+				buttons.x = FlxG.save.data.buttons[tempCount].x;
+				buttons.y = FlxG.save.data.buttons[tempCount].y;
+			}
+			tempCount++;
+		}
+
+		return touchPad;
+	}
+	
+	static function set_mode(mode:Int = 3)
+	{
+		FlxG.save.data.mobileControlsMode = mode;
+		FlxG.save.flush();
+		return mode;
+	}
+
+	static function get_mode():Int
+	{
+		if (forcedMode != null)
+			return forcedMode;
+
+		if (FlxG.save.data.mobileControlsMode == null)
+		{
+			FlxG.save.data.mobileControlsMode = 3;
+			FlxG.save.flush();
+		}
+
+		return FlxG.save.data.mobileControlsMode;
+	}
+}
+
+typedef TouchButtonsData =
+{
+	buttons:Array<ButtonsData>
+}
+
+typedef ButtonsData =
+{
+	button:String, // what TouchButton should be used, must be a valid TouchButton var from TouchPad as a string.
+	graphic:String, // the graphic of the button, usually can be located in the TouchPad xml .
+	x:Float, // the button's X position on screen.
+	y:Float, // the button's Y position on screen.
+	color:String // the button color, default color is white.
+}
+#end
